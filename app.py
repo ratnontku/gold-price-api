25d0e9cc84fe09fd1021ac06588ccc511854b4dc
ratnontku <ratchanont.ku@gmail.com>
2025-07-30 14:26:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_gold_price():
         gold_price_xpath = '//*[@id="DetailPlace_uc_goldprices1_lblOMBuy"]/b/font'
 
         price_element = tree.xpath(gold_price_xpath)
-        print(gold_price[0].text_content().strip() if gold_price else "No buy price found")
+        # print(gold_price[0].text_content().strip() if gold_price else "No buy price found") #
 
 price_text = price_element.text.strip()  # Example: '49,982.52'
 price_text = price_text.replace(',', '')  # Remove commas
