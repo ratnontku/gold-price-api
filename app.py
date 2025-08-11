8d83410be70c1e9932fca32a07cb05f3fa818acb
ratnontku <ratchanont.ku@gmail.com>
2025-08-12 00:12:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,17 +18,17 @@ def get_gold_price():
         tree = html.fromstring(response.content)
          # XPath for date and gold price table rows
         goldBar_buy_price_xpath = '//*[@id="DetailPlace_uc_goldprices1_lblOMBuy"]/b/font'
-		goldJew_buy_price_xpath = '//*[@id="DetailPlace_uc_goldprices1_lblBLBuy"]/b/font'
-		goldJew_sell_price_xpath = '//*[@id="DetailPlace_uc_goldprices1_lblBLSell"]/b/font'
+        goldJew_buy_price_xpath = '//*[@id="DetailPlace_uc_goldprices1_lblBLBuy"]/b/font'
+        goldJew_sell_price_xpath = '//*[@id="DetailPlace_uc_goldprices1_lblBLSell"]/b/font'
 
         goldBar_buy_price = tree.xpath(goldBar_buy_price_xpath)
-        print(goldBar_buy_price[0].text_content().strip() if gold_price else "No gold bar buy price found")
+        print(goldBar_buy_price[0].text_content().strip() if goldBar_buy_price else "No gold bar buy price found")
 		
-		goldJew_buy_price = tree.xpath(goldJew_buy_price_xpath)
-        print(goldJew_buy_price[0].text_content().strip() if gold_price else "No gold jew buy price found")
+        goldJew_buy_price = tree.xpath(goldJew_buy_price_xpath)
+        print(goldJew_buy_price[0].text_content().strip() if goldJew_buy_price else "No gold jew buy price found")
 		
-		goldJew_sell_price = tree.xpath(goldJew_sell_price_xpath)
-        print(goldJew_sell_price[0].text_content().strip() if gold_price else "No gold jew sell price found")
+        goldJew_sell_price = tree.xpath(goldJew_sell_price_xpath)
+        print(goldJew_sell_price[0].text_content().strip() if goldJew_sell_price else "No gold jew sell price found")
 
         return jsonify({"goldBarBuyPrice": float(goldBar_buy_price[0].text_content().strip().replace(",", ""))}, {"goldJewBuyPrice": float(goldJew_buy_price[0].text_content().strip().replace(",", ""))}, {"goldJewSellPrice": float(goldJew_sell_price[0].text_content().strip().replace(",", ""))})
 
